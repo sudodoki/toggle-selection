3f152b1a1d48093fe966def7eacc4e02ad1610ab
Peter V <vp2177@gmail.com>
2016-02-08 00:29:23 +0000

    Convert to ES6

--- toggle-selection.js
+++ toggle-selection.js
@@ -1,48 +1,38 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports.default = module.exports = factory();
-    } 
-}(this, function () {
-    function toggle() {
-      var selection = document.getSelection();
-      if (!selection.rangeCount) {
-        return function () {};
-      }
-      var active = document.activeElement;
-    
-      var ranges = [];
-      for (var i = 0; i < selection.rangeCount; i++) {
-        ranges.push(selection.getRangeAt(i));
-      }
-    
-      switch (active.tagName.toUpperCase()) { // .toUpperCase handles XHTML
-        case 'INPUT':
-        case 'TEXTAREA':
-          active.blur();
-          break;
-    
-        default:
-          active = null;
-          break;
-      }
-    
-      selection.removeAllRanges();
-      return function () {
-        selection.type === 'Caret' &&
-        selection.removeAllRanges();
-    
-        if (!selection.rangeCount) {
-          ranges.forEach(function(range) {
-            selection.addRange(range);
-          });
-        }
-    
-        active &&
-        active.focus();
-      };
+export default function toggle() {
+  var selection = document.getSelection();
+  if (!selection.rangeCount) {
+    return function () {};
+  }
+  var active = document.activeElement;
+
+  var ranges = [];
+  for (var i = 0; i < selection.rangeCount; i++) {
+    ranges.push(selection.getRangeAt(i));
+  }
+
+  switch (active.tagName.toUpperCase()) { // .toUpperCase handles XHTML
+    case 'INPUT':
+    case 'TEXTAREA':
+      active.blur();
+      break;
+
+    default:
+      active = null;
+      break;
+  }
+
+  selection.removeAllRanges();
+  return function () {
+    selection.type === 'Caret' &&
+    selection.removeAllRanges();
+
+    if (!selection.rangeCount) {
+      ranges.forEach(function(range) {
+        selection.addRange(range);
+      });
     }
-    
-    return toggle;
-}));
+
+    active &&
+    active.focus();
+  };
+}
